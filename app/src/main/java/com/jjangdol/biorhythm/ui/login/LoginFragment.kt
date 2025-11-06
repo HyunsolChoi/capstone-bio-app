704834da6e12bf049749b579721f89fb24aea211
HyunsolChoi <lasang2@naver.com>
2025-11-06 21:29:50 +0900

    Add Login-History Logic

--- app/src/main/java/com/jjangdol/biorhythm/ui/login/LoginFragment.kt
+++ app/src/main/java/com/jjangdol/biorhythm/ui/login/LoginFragment.kt
@@ -1,20 +1,27 @@
 package com.jjangdol.biorhythm.ui.login
 
 import android.os.Bundle
+import android.provider.Settings
 import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.firestore.SetOptions
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.functions.FirebaseFunctions
+import com.google.firebase.ktx.Firebase
 import com.jjangdol.biorhythm.R
 import com.jjangdol.biorhythm.databinding.FragmentLoginBinding
+import java.security.MessageDigest
+import java.util.Objects.toString
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = _binding!!
     private lateinit var functions: FirebaseFunctions
+    private val db = Firebase.firestore
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         _binding = FragmentLoginBinding.bind(view)
@@ -40,8 +47,7 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
                 "empNum" to empNum
             )
 
-            functions
-                .getHttpsCallable("loginChecker")
+            functions.getHttpsCallable("loginChecker")
                 .call(data)
                 .addOnSuccessListener { result ->
                     val res = result.data as Map<*, *>
@@ -54,6 +60,32 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
                             .putString("user_name", userName)
                             .putString("emp_num", empNum)
                             .apply()
+
+                        // Android ID 가져오기
+                        val androidId = Settings.Secure.getString(requireContext().contentResolver, Settings.Secure.ANDROID_ID)
+
+                        // firestore에 저장 시에는 해쉬된 값을 저장하도록 함.
+                        fun sha256(input: String): String {
+                            val md = MessageDigest.getInstance("SHA-256")
+                            val digest = md.digest(input.toByteArray(Charsets.UTF_8))
+                            return digest.joinToString("") { "%02x".format(it) }
+                        }
+
+                        val androidIdHash = sha256(androidId)
+
+                        val currentDate = java.text.SimpleDateFormat("yyyy-MM-dd", java.util.Locale.getDefault()).format(java.util.Date())
+                        val currentTime = java.text.SimpleDateFormat("HH:mm:ss", java.util.Locale.getDefault()).format(java.util.Date())
+
+                        // firestore에 저장 (현재 시간과 사번을 기준으로 하여 필드명을 고유하게 하고 그 안에 안드로이드 id가 남게함)
+                        db.collection("LoginHistory")
+                            .document(currentDate)
+                            .set(mapOf("$currentTime $empNum" to androidIdHash), SetOptions.merge())
+                            .addOnSuccessListener {
+                                Log.d("LoginHistory", "로그인 기록 저장 완료")
+                            }
+                            .addOnFailureListener { e ->
+                                Log.e("LoginHistory", "로그인 기록 저장 실패", e)
+                            }
 
                         Toast.makeText(requireContext(), "로그인 성공!", Toast.LENGTH_SHORT).show()
                         findNavController().navigate(R.id.action_login_to_main)
